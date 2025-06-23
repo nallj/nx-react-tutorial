daa154be3d6893c73d0760a95e0876035cbe7748
James Nall <me@jamesbnall.com>
2025-06-23 17:23:17 -0500

    add linting rules and update netlify cli

--- libs/products/src/lib/products.tsx
+++ libs/products/src/lib/products.tsx
@@ -1,6 +1,9 @@
 import styles from './products.module.css';
 import { ReactMonorepoUi } from '@react-monorepo/ui';
 
+// This import is not allowed 👇
+import { ReactMonorepoOrders } from '@react-monorepo/orders';
+
 export function ReactMonorepoProducts() {
   return (
     <div className={styles['container']}>
