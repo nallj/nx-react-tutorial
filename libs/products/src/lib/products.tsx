fabf187590e6679b4a8e811ec1d3d25d0c7cbcfd
James Nall <me@jamesbnall.com>
2025-06-23 17:46:52 -0500

    fix linter and test breakers

--- libs/products/src/lib/products.tsx
+++ libs/products/src/lib/products.tsx
@@ -1,13 +1,10 @@
 import styles from './products.module.css';
 import { ReactMonorepoUi } from '@react-monorepo/ui';
 
-// This import is not allowed 👇
-import { ReactMonorepoOrders } from '@react-monorepo/orders';
-
 export function ReactMonorepoProducts() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Products!</h1>
+      <h2>Welcome to Products!</h2>
       <p>This is a change. 👋</p>
       <ReactMonorepoUi />
     </div>
--- libs/shared/ui/src/lib/ui.tsx
+++ libs/shared/ui/src/lib/ui.tsx
@@ -3,7 +3,7 @@ import styles from './ui.module.css';
 export function ReactMonorepoUi() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to ReactMonorepoUi!</h1>
+      <h3>Welcome to ReactMonorepoUi!</h3>
     </div>
   );
 }
